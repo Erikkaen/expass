01b426f34ced37bbe8a1779574e7f771c49eb392
ErikkaSteen <erioas@hotmail.no>
2025-09-04 15:01:01 +0200

    Initial commit

---
+++ Assignment/Assignment/src/test/java/Spring/Assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
